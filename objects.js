6b553b1f00ca3c91d39783fddfeb52eab3e93ef0
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-03 20:38:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = {Mozart:"5th"}
 
 function updatePlaylist(playlist, artistName, songTitle){
- playlist.artistName = songTitle
+ playlist[artistName] = songTitle
  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
- delete playlist.artistName 
+ delete playlist[artistName]
 }
